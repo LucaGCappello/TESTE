6ee595e6d4f1edb7ff30607d144d4e8ec88bef20
LucaGCappello <152432062+LucaGCappello@users.noreply.github.com>
2025-02-17 11:38:43 +0000

    fixed bugs

--- src/components/TC.jsx
+++ src/components/TC.jsx
@@ -7,12 +7,22 @@ const TC = ({state, tasks, status, handleDelete }) => {
     <section className='Task_Column'>
         <h2 className='task_column_heading'>{state}</h2>
 
-        {
-          tasks.map((task, index) => task.status === status && <TaskCard key={index} task={task} title={task.task} 
-          tags={task.tags}
-          handleDelete= {handleDelete}
-          index={index}/>)
-        }
+        {tasks?.length > 0 ? (
+  tasks.map((task, index) => (
+    task?.status === status && (
+      <TaskCard 
+        key={index} 
+        task={task} 
+        title={task?.task} 
+        tags={task?.tags} 
+        handleDelete={handleDelete} 
+        index={index}
+      />
+    )
+  ))
+) : (
+  <p>Nenhuma tarefa encontrada</p>
+)}
         </section>
 
   )}
