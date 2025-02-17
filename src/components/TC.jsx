75ad860d1be9c2bb9c137716b434443bcfa82a19
LucaGCappello <152432062+LucaGCappello@users.noreply.github.com>
2025-02-17 09:26:23 +0000

    Saving local changes before pulling

--- src/components/TC.jsx
+++ src/components/TC.jsx
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> 3c64a77 (Last comm)
 import React from 'react'
 import TaskCard from './TaskCard'
 
@@ -6,6 +11,7 @@ const TC = ({state, tasks, status, handleDelete }) => {
   return (
     <section className='Task_Column'>
         <h2 className='task_column_heading'>{state}</h2>
+<<<<<<< HEAD
         {
           tasks.map((task, index) => task.status === status && <TaskCard key={index} task={task} title={task.task} 
           tags={task.tags}
@@ -13,6 +19,16 @@ const TC = ({state, tasks, status, handleDelete }) => {
           index={index}/>)
         }
         </section>
+=======
+            {
+           tasks.map((task, index) => task.status === status && <TaskCard key={index} task={task} title={task.task} 
+           tags={task.tags}
+         handleDelete= {handleDelete}
+          index={index}/>)
+        }
+        </section>
+    
+>>>>>>> 3c64a77 (Last comm)
   )}
 
 
