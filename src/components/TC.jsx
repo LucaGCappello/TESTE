4d4e051cf1bf9335bb7dcd04182e6f597eb5da0e
LucaGCappello <152432062+LucaGCappello@users.noreply.github.com>
2025-02-17 09:31:13 +0000

    commit

--- src/components/TC.jsx
+++ src/components/TC.jsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 3c64a77 (Last comm)
 import React from 'react'
 import TaskCard from './TaskCard'
 
@@ -11,7 +6,7 @@ const TC = ({state, tasks, status, handleDelete }) => {
   return (
     <section className='Task_Column'>
         <h2 className='task_column_heading'>{state}</h2>
-<<<<<<< HEAD
+
         {
           tasks.map((task, index) => task.status === status && <TaskCard key={index} task={task} title={task.task} 
           tags={task.tags}
@@ -19,16 +14,7 @@ const TC = ({state, tasks, status, handleDelete }) => {
           index={index}/>)
         }
         </section>
-=======
-            {
-           tasks.map((task, index) => task.status === status && <TaskCard key={index} task={task} title={task.task} 
-           tags={task.tags}
-         handleDelete= {handleDelete}
-          index={index}/>)
-        }
-        </section>
-    
->>>>>>> 3c64a77 (Last comm)
+
   )}
 
 
